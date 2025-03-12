01f0dd3cb3b536d6047e1d82660ac88d0303696f
Roger-kuku <rogerlee2510@gmail.com>
2025-03-12 12:05:56 +0000

    Update PIR_Sensor.cpp

--- codes/PIR_Sensor.cpp
+++ codes/PIR_Sensor.cpp
@@ -1,26 +1,51 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include "wiringPi.h"
+#include <iostream>
+#include <gpiod.h>
+#include <chrono>
+#include <thread>
 
-int main( )
-{
-    if(wiringPiSetup() == -1){
-        printf("%s \n", "Failed to setup wiringPi");
-    }
-    
-   	pinMode(1, INPUT); // set pin 1 as input
+#define GPIO_CHIP "/dev/gpiochip0"  // GPIO chip device file
+#define PIR_SENSOR_PIN 17           // GPIO pin number
 
-    // we need to activate the pull-up resistor to
-    // make use of this specific PIR sensor
-	pullUpDnControl(1, PUD_UP); 
-    
-    while(1) {
-        // read the value
-        // 1 means no movement
-        // 0 means that something is moving
-        int val = digitalRead(1);
-        printf("%s: %i\n", "sensor", val );
-        usleep(100000); // 10fps
+int main() {
+    // Open the GPIO chip
+    gpiod_chip *chip = gpiod_chip_open(GPIO_CHIP);
+    if (!chip) {
+        std::cerr << "Failed to open GPIO chip" << std::endl;
+        return 1;
     }
+
+    // Get the line corresponding to the PIR sensor pin
+    gpiod_line *line = gpiod_chip_get_line(chip, PIR_SENSOR_PIN);
+    if (!line) {
+        std::cerr << "Failed to get GPIO line" << std::endl;
+        gpiod_chip_close(chip);
+        return 1;
+    }
+
+    // Request the line as input
+    if (gpiod_line_request_input(line, "pir_sensor") < 0) {
+        std::cerr << "Failed to request GPIO line as input" << std::endl;
+        gpiod_chip_close(chip);
+        return 1;
+    }
+
+    while (true) {
+        // Read the PIR sensor state
+        int val = gpiod_line_get_value(line);
+        if (val < 0) {
+            std::cerr << "Failed to read GPIO value" << std::endl;
+            break;
+        }
+
+        // Print the sensor value
+        std::cout << "Sensor: " << val << std::endl;
+
+        // Sleep for 100ms (10fps)
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    // Clean up
+    gpiod_line_release(line);
+    gpiod_chip_close(chip);
+    return 0;
 }

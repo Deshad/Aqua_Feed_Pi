16be8be7db1a7a9f8d99af00a8eecb48d0f4825c
Deshad <gagan.deshad@gmail.com>
2025-04-01 18:43:54 +0100

    main_codes_api

---
+++ main_codes/src/CMakeFiles/fish_monitor.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/team35/Aqua_Feed_Pi/Main/src/camera.cpp" "CMakeFiles/fish_monitor.dir/camera.cpp.o" "gcc" "CMakeFiles/fish_monitor.dir/camera.cpp.o.d"
+  "/home/team35/Aqua_Feed_Pi/Main/src/feeder.cpp" "CMakeFiles/fish_monitor.dir/feeder.cpp.o" "gcc" "CMakeFiles/fish_monitor.dir/feeder.cpp.o.d"
+  "/home/team35/Aqua_Feed_Pi/Main/src/fish_api.cpp" "CMakeFiles/fish_monitor.dir/fish_api.cpp.o" "gcc" "CMakeFiles/fish_monitor.dir/fish_api.cpp.o.d"
+  "/home/team35/Aqua_Feed_Pi/Main/src/fish_monitoring_system.cpp" "CMakeFiles/fish_monitor.dir/fish_monitoring_system.cpp.o" "gcc" "CMakeFiles/fish_monitor.dir/fish_monitoring_system.cpp.o.d"
+  "/home/team35/Aqua_Feed_Pi/Main/src/image_processor.cpp" "CMakeFiles/fish_monitor.dir/image_processor.cpp.o" "gcc" "CMakeFiles/fish_monitor.dir/image_processor.cpp.o.d"
+  "/home/team35/Aqua_Feed_Pi/Main/src/main.cpp" "CMakeFiles/fish_monitor.dir/main.cpp.o" "gcc" "CMakeFiles/fish_monitor.dir/main.cpp.o.d"
+  "/home/team35/Aqua_Feed_Pi/Main/src/motor.cpp" "CMakeFiles/fish_monitor.dir/motor.cpp.o" "gcc" "CMakeFiles/fish_monitor.dir/motor.cpp.o.d"
+  "/home/team35/Aqua_Feed_Pi/Main/src/ph_sensor.cpp" "CMakeFiles/fish_monitor.dir/ph_sensor.cpp.o" "gcc" "CMakeFiles/fish_monitor.dir/ph_sensor.cpp.o.d"
+  "/home/team35/Aqua_Feed_Pi/Main/src/pir_sensor.cpp" "CMakeFiles/fish_monitor.dir/pir_sensor.cpp.o" "gcc" "CMakeFiles/fish_monitor.dir/pir_sensor.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ main_codes/src/CMakeFiles/fish_monitor.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fish_monitor.dir/camera.cpp.o"
+  "CMakeFiles/fish_monitor.dir/camera.cpp.o.d"
+  "CMakeFiles/fish_monitor.dir/feeder.cpp.o"
+  "CMakeFiles/fish_monitor.dir/feeder.cpp.o.d"
+  "CMakeFiles/fish_monitor.dir/fish_api.cpp.o"
+  "CMakeFiles/fish_monitor.dir/fish_api.cpp.o.d"
+  "CMakeFiles/fish_monitor.dir/fish_monitoring_system.cpp.o"
+  "CMakeFiles/fish_monitor.dir/fish_monitoring_system.cpp.o.d"
+  "CMakeFiles/fish_monitor.dir/image_processor.cpp.o"
+  "CMakeFiles/fish_monitor.dir/image_processor.cpp.o.d"
+  "CMakeFiles/fish_monitor.dir/main.cpp.o"
+  "CMakeFiles/fish_monitor.dir/main.cpp.o.d"
+  "CMakeFiles/fish_monitor.dir/motor.cpp.o"
+  "CMakeFiles/fish_monitor.dir/motor.cpp.o.d"
+  "CMakeFiles/fish_monitor.dir/ph_sensor.cpp.o"
+  "CMakeFiles/fish_monitor.dir/ph_sensor.cpp.o.d"
+  "CMakeFiles/fish_monitor.dir/pir_sensor.cpp.o"
+  "CMakeFiles/fish_monitor.dir/pir_sensor.cpp.o.d"
+  "fish_monitor"
+  "fish_monitor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fish_monitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
